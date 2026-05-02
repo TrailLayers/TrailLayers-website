8927d41bf6af3a32a69f6fda8fb2e7fbb59d408b
Antonio Pelayo <aPelayo.py@gmail.com>
2026-05-02 08:00:59 -0700

    feat: update beta signup buttons to App Store link

--- shared/constants.js
+++ shared/constants.js
@@ -1,3 +1,3 @@
 export const SUPABASE_URL = "https://aumkrmgkdhnkkjdwzvdp.supabase.co";
 export const CDN_BASE = "https://cdn.traillayers.app";
-export const APP_STORE_URL = "https://tally.so/r/9q6ayX";
+export const APP_STORE_URL = "https://apps.apple.com/us/app/traillayers/id6760794709";
